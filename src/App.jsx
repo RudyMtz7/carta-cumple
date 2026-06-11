e8fdc1898ece2b60d80cac6faadd87b8398f526a
rodolfo.martinez1 <rodolfo.martinez@petco.com>
2026-06-10 21:40:27 -0600

    Terminar carta

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,45 @@ import HeartBackground from "./HeartBackground";
 
 const paragraphs = [
   {
-    title: "To My Favorite Person",
+    title: "hace 21 añitos...",
     image: `${import.meta.env.BASE_URL}images/regi_trix.png`,
-    text: `Somehow, in a world full of people, I found you. What started as ordinary conversations became the part of my day I looked forward to the most. You have a way of making even the smallest moments feel meaningful, and for that, I am endlessly grateful.`
+    text: `llego una personita muy hermosa a este mundo`
   },
   {
-    title: "The Little Things",
-    image: `${import.meta.env.BASE_URL}images/toki.png`,
-    text: `It's not just the big memories I treasure. It's the random jokes, the late-night conversations, the comfortable silence, and the way you make everything feel lighter. Those moments have become some of my favorite memories.`
+    title: "y hace 3 meses",
+    image: `${import.meta.env.BASE_URL}images/ipad.png`,
+    text: `ella entro a mi vida y la cambio por completo`
   },
   {
-    title: "Looking Forward",
-    text: `No one knows exactly what the future holds, but I know this: every plan, dream, and adventure seems better when I imagine you there. Thank you for being such an important part of my life. I love you.`
-  }
+    image: `${import.meta.env.BASE_URL}images/toiki.png`,
+    text: `hoy quiero celebrar tu vida amor. Porque se lo especial que es este día para ti y quiero hacerte la más feliz. Como tu lo haces todos los días.`
+  },
+  {
+    title: "llenarte de regalos",
+    image: `${import.meta.env.BASE_URL}images/ramo.png`,
+    text: `te quiero consentir en tu festejo y darte muchas cositas que se que te gustan`
+  },
+  {
+    title: "quiero estar contigo",
+    image: `${import.meta.env.BASE_URL}images/boda.png`,
+    text: `
+      los lunes 
+      los fines
+      todos los días
+      ahora
+      y siempre
+      pero en especial
+      hoy
+    `
+  },
+  {
+    title: "asi que te dejo con algo nuevo",
+    image: `${import.meta.env.BASE_URL}images/nazca.png`,
+    text: `algo que se me ocurrio pensando en todas las cartas que te he escrito. Todas las flores que te he dado.
+    y todos los detalitos que me encanta hacerte. 
+    
+    para poder recorrer algunos de los muchos momentos que hemos vivido en estos 100 días juntos (si, exactamente 100 días de nuestra primera cita en Hercules <3)`
+  },
 ];
 
 export default function App() {
@@ -29,7 +55,6 @@ export default function App() {
           <section className="hero-panel">
             <div className="hero-content">
               <h1 className="hero-title">Para regi</h1>
-              {/*<div className="hero-heart">❤️</div>*/}
               <p className="hero-subtitle">15 de junio del 2026</p>
               <span className="scroll-hint">↓ Scroll ↓</span>
             </div>
@@ -49,6 +74,28 @@ export default function App() {
             </div>
           </section>
         ))}
+        <section className="hero-panel">
+            <div className="hero-content">
+              <h1 className="hero-title">y una última cosa</h1>
+            </div>
+        </section>
+        <section className="final-panel">
+          <h2 className="final-top-title">
+            espero te guste este joven. porque lo vamos a ver en unos meses
+          </h2>
+
+          <img
+            className="final-big-image"
+            src={`${import.meta.env.BASE_URL}images/boletos.PNG`}
+            alt="Boletos"
+          />
+
+          <h2 className="final-bottom-title">
+            feliz cumpleaños regina ❤️
+            Te amo
+          </h2>
+          <h1 className="hero-title">-Rodolfo</h1>
+        </section>
       </div>
     </div>
   );
